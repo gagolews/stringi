39046886dc2ada064c2ae5aca59ac7beb06ee4b0
gagolews <egzaltowany@gmail.com>
2013-01-05 17:49:47 +0100

    add test case

--- inst/tests/test-trim.R
+++ inst/tests/test-trim.R
@@ -1,3 +1,14 @@
 require(testthat)
 
 
+test_that("stri_trim", {
+  
+   expect_that(stri_trim("      "), is_equivalent_to(""))
+   expect_that(stri_trim("   A  "), is_equivalent_to("A"))
+   expect_that(stri_trim("A     "), is_equivalent_to("A"))
+   expect_that(stri_trim("     A"), is_equivalent_to("A"))
+   
+   expect_that(stri_trim(LETTERS), is_equivalent_to(LETTERS))
+   
+   expect_that(stri_trim(stri_dup(" ", 1:100)), is_equivalent_to(rep("", 100)))
+})
