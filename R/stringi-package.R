7b8006db50c1899b1acffd9a31a5b2dacae59bab
gagolews <egzaltowany@gmail.com>
2013-01-05 22:17:01 +0100

    add copyright/license info

--- R/stringi-package.R
+++ R/stringi-package.R
@@ -1,4 +1,26 @@
-#' aa
+## This file is part of the 'stringi' library.
+##
+## Copyright 2013 Marek Gagolewski, Bartek Tartanus
+##
+##
+## 'stringi' is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## 'stringi' is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with 'stringi'. If not, see <http://www.gnu.org/licenses/>.
+
+
+
+## TO DO: package doc
+
+#' TO DO
 #' @useDynLib stringi
 .onAttach <- function(lib, pkg)
 {
