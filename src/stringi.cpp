290beef04713f74101704af39788d19ab19de95b
bartektartanus <bartektartanus@gmail.com>
2013-01-09 22:09:03 +0100

    Merged two different versions

--- src/stringi.cpp
+++ src/stringi.cpp
@@ -28,24 +28,6 @@
  * \code{\{"method_name", (DL_FUNC)pointer, number_of_args\}}
  */
 static const R_CallMethodDef cCallMethods[] = {
-<<<<<<< HEAD
-   {"stri_trim", (DL_FUNC)&stri_trim, 1},
-   {"stri_dup", (DL_FUNC)&stri_dup, 2},
-   {"stri_join2", (DL_FUNC)&stri_join2, 2},
-   {"stri_flatten", (DL_FUNC)&stri_flatten, 1},
-   {"stri_unicode_normalization", (DL_FUNC)&stri_unicode_normalization, 2},
-   {"stri_casefold", (DL_FUNC)&stri_casefold, 2},
-   {"stri_getinfo", (DL_FUNC)&stri_getinfo, 0},
-   {"stri_prepare_arg_string", (DL_FUNC)&stri_prepare_arg_string, 1},
-   {"stri_casecompare", (DL_FUNC)&stri_casecompare, 2},
-   {"stri_ucnv_enclist", (DL_FUNC)&stri_ucnv_enclist, 0},
-   {"stri_ucnv_encinfo", (DL_FUNC)&stri_ucnv_encinfo, 1},
-   {"stri_numbytes", (DL_FUNC)&stri_numbytes, 1},
-   {"stri_wrap_greedy", (DL_FUNC)&stri_wrap_greedy, 3},
-   {"stri_wrap_dynamic", (DL_FUNC)&stri_wrap_dynamic, 3},
-   {"stri_split", (DL_FUNC)&stri_split, 1},
-   {NULL, NULL, 0}
-=======
    
    {"stri_casecompare",             (DL_FUNC)&stri_casecompare,            2},
    {"stri_casefold",                (DL_FUNC)&stri_casefold,               2},
@@ -56,6 +38,7 @@ static const R_CallMethodDef cCallMethods[] = {
    {"stri_numbytes",                (DL_FUNC)&stri_numbytes,               1},
    {"stri_prepare_arg_string",      (DL_FUNC)&stri_prepare_arg_string,     1},
    {"stri_prepare_arg_integer",     (DL_FUNC)&stri_prepare_arg_integer,    1},
+   {"stri_split",							(DL_FUNC)&stri_split, 						1},
    {"stri_trim",                    (DL_FUNC)&stri_trim,                   1},
    {"stri_ucnv_enclist",            (DL_FUNC)&stri_ucnv_enclist,           0},
    {"stri_ucnv_encinfo",            (DL_FUNC)&stri_ucnv_encinfo,           1},
@@ -66,7 +49,7 @@ static const R_CallMethodDef cCallMethods[] = {
    
    // the list must be NULL-terminated:
    {NULL,                           NULL,                                  0}
->>>>>>> 430a6ef76e7f0ec017fca5adb3e524f1eec66cc5
+
 };
 
 
