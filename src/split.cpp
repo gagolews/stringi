306ffeff20a5858f24d4c7489a240e1d1aad5b4c
gagolews <egzaltowany@gmail.com>
2013-01-10 09:54:34 +0100

    some suggestions :)

--- src/split.cpp
+++ src/split.cpp
@@ -27,7 +27,7 @@ SEXP stri_split(SEXP s)
 {
    s = stri_prepare_arg_string(s);
    int n = LENGTH(s);
-   int* count = (int*)R_alloc(n, sizeof(int)); 
+   int* count = (int*)R_alloc(n, sizeof(int)); // this is not needed - it can be a scalar variable
    SEXP e;
    PROTECT(e = allocVector(VECSXP,n));
    SEXP curs,temp;
@@ -36,20 +36,35 @@ SEXP stri_split(SEXP s)
    	curs = STRING_ELT(s, i);
    	k = LENGTH(curs);
    	count[i]=0;
+      // const char* string = CHAR(curs);
    	for(int j=0; j<k; ++j){
-   		b = (int)CHAR(curs)[j];
-   		if(b==10)
+   		b = (int)CHAR(curs)[j]; // just string[i] - it's fast, no b needed
+   		if(b==10) // MG: or b == '\n' ('a' - char, "a" - string (char*): {'a', '\0'})
 				count[i]++;
+         // MG: what if string[i] == '\n' and string[i-1] == '\n' (i>0) ? 
+         // don't increment count in such case (?)
    	}
    	PROTECT(temp = allocVector(STRSXP,count[i]+1));
    	printf("count%d=%d ",i,count[i]);
    	st=0;
    	where=0;
-   	for(int j=0; j<k; ++j){
-			b = (int)CHAR(curs)[j];
-   			if(b==10){
+   	for(int j=0; j<k; ++j){ // j<count[i]?
+			b = (int)CHAR(curs)[j]; // see above
+   			if(b==10){ // '\n'
    				end=j;
    				SET_STRING_ELT(temp,where, curs);//substr(STRING_ELT(curs,0),st,end));
+               // MG: http://www.cplusplus.com/reference/cstring/
+               
+               //given start and end - index in string
+               //we can do:
+               // SET_STRING_ELT(temp, j, mkCharLen(string+start, end-start+1))
+               // string - address of the first character
+               // string+start - address of the start-th character
+               // definition: mkCharLen(char* address_of_first_char, int howManyCharsToCopy)
+               // - it returns a "scalar" string that may be copied into STRSXP (with SET_STRING_ELT)
+               // good work - i'm glad it's challenging :)
+               
+               
    				st=j;
    				++where;
    			}
