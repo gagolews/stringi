baa9b6c6fe5f5d6af619e55bb1d020ecf0dbc5f0
gagolews <egzaltowany@gmail.com>
2013-01-16 21:47:00 +0100

    Package authors updated

--- R/stringi-package.R
+++ R/stringi-package.R
@@ -1,6 +1,6 @@
 ## This file is part of the 'stringi' library.
 ##
-## Copyright 2013 Marek Gagolewski, Bartek Tartanus
+## Copyright 2013 Marek Gagolewski, Bartek Tartanus, Marcin Bujarski
 ##
 ##
 ## 'stringi' is free software: you can redistribute it and/or modify
@@ -10,7 +10,7 @@
 ##
 ## 'stringi' is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ## GNU Lesser General Public License for more details.
 ##
 ## You should have received a copy of the GNU Lesser General Public License
@@ -27,7 +27,8 @@
 #' @docType package
 #' @title Correct, fast, and simple string manipulation in each locale
 #' @author Marek Gagolewski \email{gagolews@@ibspan.waw.pl},\cr
-#' Bartek Tartanus \email{bartektartanus@@gmail.com}
+#' Bartek Tartanus \email{bartektartanus@@gmail.com},\cr
+#' Marcin Bujarski \email{marcin.bujarski@@gmail.com}
 #' @references
 #' Package homepage, \url{http://www.ibspan.waw.pl/~gagolews/?page=resources&subpage=stringi}.\cr
 #' ICU - International Components for Unicode, \url{http://www.icu-project.org/}.\cr
