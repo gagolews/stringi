3c54ce1fcfbc7c12b647191de4884e38502e594c
bartektartanus <bartektartanus@gmail.com>
2013-06-10 13:36:32 +0200

    ADDED: some tests for stri_locate_last_fixed

--- inst/tests/test-locate-fixed.R
+++ inst/tests/test-locate-fixed.R
@@ -81,6 +81,31 @@ test_that("stri_locate_last_fixed", {
    expect_equivalent(stri_locate_last_fixed(NA, ""), matrix(c(NA,NA_integer_)))
    
    expect_equivalent(stri_locate_last_fixed("1a\u0105a", "\u0105"), matrix(c(3,3)))
+   #overlapping pattern
    expect_equivalent(stri_locate_last_fixed("aaa", "aa"), matrix(c(2,3)))
+   expect_equivalent(stri_locate_last_fixed("aa1a12aa123", "123"), matrix(c(9,11)))
+   expect_equivalent(stri_locate_last_fixed("1-1-2-33--2", "-32"), matrix(c(NA_integer_,NA_integer_)))
+   
+   expect_equivalent(stri_locate_last_fixed("\u0105a", "\u0105a"), matrix(c(1,2)))
+   expect_equivalent(stri_locate_last_fixed(stri_enc_nfkd("\u0105a"), "\u0105a"), matrix(c(1,3)))
+   expect_equivalent(stri_locate_last_fixed("\U0001F0A0a", "a"), matrix(c(2,2)))
+   
+   #byte
+   expect_is(stri_locate_last_fixed(character(0), "a", NA), "matrix")
+   
+   expect_equivalent(nrow(stri_locate_last_fixed(NA, character(0), NA)), 0)
+   expect_equivalent(nrow(stri_locate_last_fixed(character(0), NA, NA)), 0)
+   expect_equivalent(nrow(stri_locate_last_fixed(LETTERS, character(0), NA)), 0)
+   expect_equivalent(stri_locate_last_fixed(NA, "", NA), matrix(c(NA_integer_,NA_integer_)))
+   
+   expect_equivalent(stri_locate_last_fixed("1a\u0105a", "\u0105", NA), matrix(c(3,3)))
+   #overlapping pattern
+   expect_equivalent(stri_locate_last_fixed("aaa", "aa", NA), matrix(c(2,3)))
+   expect_equivalent(stri_locate_last_fixed("aa1a12aa123", "123", NA), matrix(c(9,11)))
+   expect_equivalent(stri_locate_last_fixed("1-1-2-33--2", "-32", NA), matrix(c(NA_integer_,NA_integer_)))
+   
+   expect_equivalent(stri_locate_last_fixed("\u0105a", "\u0105a", NA), matrix(c(1,2)))
+   expect_equivalent(stri_locate_last_fixed(stri_enc_nfkd("\u0105a"), "\u0105a", NA), matrix(c(NA_integer_,NA)))
+   expect_equivalent(stri_locate_last_fixed("\U0001F0A0a", "a", NA), matrix(c(2,2)))
    
 })
