0b2f631e13541b4b1620809d13de85931b33202b
gagolews <egzaltowany@gmail.com>
2013-06-17 23:51:37 +0200

    ``ss`` fix in search_regex_count.cpp

--- src/search_regex_count.cpp
+++ src/search_regex_count.cpp
@@ -3,16 +3,16 @@
  * Copyright 2013 Marek Gagolewski, Bartek Tartanus, Marcin Bujarski
  * 
  * 'stringi' is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lestr_conter General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
  * 'stringi' is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNEstr_cont FOR A PARTICULAR PURPOSE. See the
- * GNU Lestr_conter General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU Lestr_conter General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with 'stringi'. If not, see <http://www.gnu.org/licenses/>.
  */
  
