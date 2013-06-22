44a41ec7a68346e9e483cf948ed38d30ca422335
gagolews <egzaltowany@gmail.com>
2013-06-23 01:05:08 +0200

    DONE: stri_count_fixed

--- src/search_fixed_count.cpp
+++ src/search_fixed_count.cpp
@@ -30,51 +30,50 @@
  * @version 0.1 (Bartek Tartanus)
  * @version 0.2 (Marek Gagolewski) - use StriContainerUTF8
  * @version 0.3 (Marek Gagolewski) - corrected behavior on empty str/pattern
+ * @version 0.4 (Marek Gagolewski, 2013-06-23) make StriException-friendly
  */
 SEXP stri_count_fixed_byte(SEXP str, SEXP pattern)
 {
    str = stri_prepare_arg_string(str, "str");
    pattern = stri_prepare_arg_string(pattern, "pattern");
-   int ns = LENGTH(str);
-   int np = LENGTH(pattern);
-   if (ns <= 0 || np <= 0) return allocVector(INTSXP, 0);
-   int nmax = stri__recycling_rule(true, 2, ns, np);
    
-   StriContainerUTF8* ss = new StriContainerUTF8(str, nmax);
-   StriContainerUTF8* sp = new StriContainerUTF8(pattern, nmax);
+   STRI__ERROR_HANDLER_BEGIN
+   int vectorize_length = stri__recycling_rule(true, 2, LENGTH(str), LENGTH(pattern));
+   StriContainerUTF8 str_cont(str, vectorize_length);
+   StriContainerUTF8 pattern_cont(pattern, vectorize_length);
 
    SEXP ret;
-   PROTECT(ret = allocVector(INTSXP, nmax));
+   PROTECT(ret = allocVector(INTSXP, vectorize_length));
    int* ret_tab = INTEGER(ret);
    
-   for (R_len_t i=0; i<nmax; ++i) {  
-      STRI__CONTINUE_ON_EMPTY_OR_NA_STR_PATTERN(*ss, *sp, ret_tab[i] = NA_INTEGER, ret_tab[i] = 0)
+   for (R_len_t i = pattern_cont.vectorize_init();
+      i != pattern_cont.vectorize_end();
+      i = pattern_cont.vectorize_next(i))
+   {  
+      STRI__CONTINUE_ON_EMPTY_OR_NA_STR_PATTERN(str_cont, pattern_cont,
+      ret_tab[i] = NA_INTEGER, ret_tab[i] = 0)
+      
+      const char* str_cur_s      = str_cont.get(i).c_str();
+      const char* pattern_cur_s  = pattern_cont.get(i).c_str();
+      R_len_t str_cur_n          = str_cont.get(i).length();
+      R_len_t pattern_cur_n      = pattern_cont.get(i).length();
       
       ret_tab[i] = 0;
-      const String8* curs = &ss->get(i);
-      const String8* curp = &sp->get(i);
-      const char* chs = curs->c_str();
-      const char* chp = curp->c_str();
-      int ns = curs->length();
-      int np = curp->length();
-      
-      ret_tab[i] = false;
-      for (int j=0; j<ns-np+1; ++j) {                  // O(ns*np) algorithm
-         int k=0;
-         while(k<np && chs[j+k] == chp[k])
+      for (R_len_t j=0; j<str_cur_n-pattern_cur_n+1; ++j) {  
+         R_len_t k=0;
+         while (k<pattern_cur_n && str_cur_s[j+k] == pattern_cur_s[k])
             k++;
-      	if(k == np) {
+      	if (k == pattern_cur_n) {
             ++ret_tab[i];
-            //if match then skip and check next element of s
-            j += np-1; // skip next np chars (-1, as we do ++j)
+            // if match then skip and check next element of str_cur_s
+            j += pattern_cur_n-1; // skip next pattern_cur_n chars (-1, as we do ++j)
    		}
    	}
    }
    
-   delete ss;
-   delete sp;
    UNPROTECT(1);
    return ret;
+   STRI__ERROR_HANDLER_END( ;/* do nothing special on error */ )
 }
 
 
@@ -91,85 +90,50 @@ SEXP stri_count_fixed_byte(SEXP str, SEXP pattern)
  * 
  * @version 0.1 (Marek Gagolewski)
  * @version 0.2 (Marek Gagolewski) - corrected behavior on empty str/pattern
+ * @version 0.3 (Marek Gagolewski, 2013-06-23) make StriException-friendly, use StriContainerUStringSearch
  */
 SEXP stri_count_fixed(SEXP str, SEXP pattern, SEXP collator_opts)
 {
-   UCollator* col = stri__ucol_open(collator_opts);
-   if (!col)
-      return stri_count_fixed_byte(str, pattern);
-   
    str = stri_prepare_arg_string(str, "str");
    pattern = stri_prepare_arg_string(pattern, "pattern");
-   R_len_t nmax = stri__recycling_rule(true, 2, LENGTH(str), LENGTH(pattern));
+
+   // call stri__ucol_open after prepare_arg:
+   // if prepare_arg had failed, we would have a mem leak
+   UCollator* collator = stri__ucol_open(collator_opts);
+   if (!collator)
+      return stri_count_fixed_byte(str, pattern);
+   
+   STRI__ERROR_HANDLER_BEGIN
+   R_len_t vectorize_length = stri__recycling_rule(true, 2, LENGTH(str), LENGTH(pattern));
+   StriContainerUTF16 str_cont(str, vectorize_length);
+   StriContainerUStringSearch pattern_cont(pattern, vectorize_length, collator);  // collator is not owned by pattern_cont
    
    SEXP ret;
-   PROTECT(ret = allocVector(INTSXP, nmax));
+   PROTECT(ret = allocVector(INTSXP, vectorize_length));
    int* ret_tab = INTEGER(ret);
    
-   StriContainerUTF16* ss = new StriContainerUTF16(str, nmax);
-   StriContainerUTF16* pp = new StriContainerUTF16(pattern, nmax);
-   UErrorCode err = U_ZERO_ERROR;
-   
-   
-   const UnicodeString* last_str = NULL;
-   const UnicodeString* last_pat = NULL;
-   err = U_ZERO_ERROR;
-   UStringSearch *matcher = NULL;
-   
-   if (U_FAILURE(err))
-      error(MSG__STRSEARCH_FAILED);
-   
-   for (R_len_t i = pp->vectorize_init();
-         i != pp->vectorize_end();
-         i = pp->vectorize_next(i))
+   for (R_len_t i = pattern_cont.vectorize_init();
+         i != pattern_cont.vectorize_end();
+         i = pattern_cont.vectorize_next(i))
    {
-      STRI__CONTINUE_ON_EMPTY_OR_NA_STR_PATTERN(*ss, *pp, ret_tab[i] = NA_INTEGER, ret_tab[i] = 0)
+      STRI__CONTINUE_ON_EMPTY_OR_NA_STR_PATTERN(str_cont, pattern_cont,
+         ret_tab[i] = NA_INTEGER, 
+         ret_tab[i] = 0)
       
-      const UnicodeString* cur_str = &(ss->get(i));
-      const UnicodeString* cur_pat = &(pp->get(i));
-      
-      if (!matcher) {
-         last_pat = cur_pat;
-         last_str = cur_str;
-         err = U_ZERO_ERROR;
-         matcher = usearch_openFromCollator(last_pat->getBuffer(), last_pat->length(),
-            last_str->getBuffer(), last_str->length(), col, NULL, &err);
-         if (U_FAILURE(err)) error(MSG__STRSEARCH_FAILED);
-//            usearch_setAttribute(matcher, USEARCH_OVERLAP, USEARCH_OFF, &err); // this is default
-      }
-
-      if (cur_pat != last_pat) {
-         last_pat = cur_pat;
-         err = U_ZERO_ERROR;
-         usearch_setPattern(matcher, last_pat->getBuffer(), last_pat->length(), &err);
-         if (U_FAILURE(err)) error(MSG__STRSEARCH_FAILED);
-      }
-
-      if (cur_str != last_str) {
-         last_str = cur_str;
-         err = U_ZERO_ERROR;
-         usearch_setText(matcher, last_str->getBuffer(), last_str->length(), &err);
-         if (U_FAILURE(err)) error(MSG__STRSEARCH_FAILED);
-      }
-      
+      UStringSearch *matcher = pattern_cont.getMatcher(i, str_cont.get(i));
       usearch_reset(matcher);
-      err = U_ZERO_ERROR;
+      UErrorCode status = U_ZERO_ERROR;
       ret_tab[i] = 0;
-      int result = (int)usearch_first(matcher, &err); // this is F*G slow! :-(
-      if (U_FAILURE(err)) error(MSG__STRSEARCH_FAILED);
-      while (result != USEARCH_DONE && !U_FAILURE(err))
-      {
+      while (((int)usearch_next(matcher, &status) != USEARCH_DONE) && !U_FAILURE(status))
          ++ret_tab[i];
-         result = (int)usearch_next(matcher, &err);
-      }
-      if (U_FAILURE(err)) error(MSG__STRSEARCH_FAILED);
+      if (U_FAILURE(status)) throw StriException(status);
    }
    
-   if (col) ucol_close(col);
-   if (matcher) usearch_close(matcher);
-   delete ss;
-   delete pp;
+   if (collator) { ucol_close(collator); collator=NULL; }
    UNPROTECT(1);
    return ret;
+   STRI__ERROR_HANDLER_END(
+      if (collator) ucol_close(collator);
+   )
 }
 
