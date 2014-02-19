ae52869159281cc7a772c0295dd4240121c66c32
gagolews <egzaltowany@gmail.com>
2014-02-19 23:16:06 +0100

    preparing stringi for CRAN submission #6
    Former-commit-id: 2df265c75ddd9f29618cbc3d5550b8d0871d0d3a

--- src/uconfig_local.h
+++ src/uconfig_local.h
@@ -39,8 +39,12 @@
 /* avoid feature_tests.h errors */
 #if defined(_STDC_C99) && (defined(__XOPEN_OR_POSIX) && !defined(_XPG6))
 #define _XPG6
+#undef __XOPEN_OR_POSIX
+#define __XOPEN_OR_POSIX 600
 #elif !defined(_STDC_C99) && (defined(__XOPEN_OR_POSIX) && defined(_XPG6))
 #undef _XPG6
+#undef __XOPEN_OR_POSIX
+#define __XOPEN_OR_POSIX 500
 #endif
 
 #endif
