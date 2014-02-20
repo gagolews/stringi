743527be86daceaf95206a769db35fb3435c2f2f
gagolews <egzaltowany@gmail.com>
2014-02-20 12:53:51 +0100

    icu52 sources moved to i18n/ stubdata/ and common/; stringi_0.1-22 started

---
+++ devel/cat_icu_sources.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/Rscript --vanilla
+
+# must be called within src/
+
+prefix <- 'icu52_'
+
+catsrc <- function(dir, ext) {
+   lst <- dir(dir, glob2rx(paste0("*.", ext)), full.names=TRUE)
+   cat(lst, sep=' \\\n', file=paste0(prefix, dir, "_", ext, ".txt"))
+}
+
+catsrc('i18n', 'c')
+catsrc('i18n', 'cpp')
+catsrc('common', 'c')
+catsrc('common', 'cpp')
+catsrc('stubdata', 'c')
+catsrc('stubdata', 'cpp')
+
+lst <- dir('.', glob2rx(paste0("*.cpp")))
+cat(lst, sep=' \\\n', file=paste0("stri_cpp.txt"))
+
