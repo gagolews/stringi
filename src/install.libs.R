0a8eccce5c44e394a034ef7a24fc4a3fb72c6581
gagolews <egzaltowany@gmail.com>
2014-02-20 16:18:28 +0100

    fixing Windows building; more intelligent tests for proper icu>=50 installation

--- src/install.libs.R
+++ src/install.libs.R
@@ -4,11 +4,17 @@ dest <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
 dir.create(dest, recursive=TRUE, showWarnings=FALSE)
 file.copy(libfile, dest, overwrite=TRUE)
 
-# copy icudt on little-endian systems:
+# copy icudt on little-endian systems AND in case of building ICU from sources
+copyicudt <- TRUE
+if (file.exists('icu52_found.txt')) {
+   copyicudt <- !as.logical(as.numeric(readChar('icu52_found.txt', 1)))
+   if (is.na(copyicudt)) copyicudt <- TRUE
+}
+
 icudtpath <- 'icu52/data/icudt52l.dat'
-copyicudt <- !as.logical(0) # see ./configure
 if (copyicudt && .Platform$endian == 'little' && file.exists(icudtpath)) {
-   # we're shipping stringi only with little-endian ASCII version of icudt
+   # we are shipping stringi only with little-endian ASCII version of icudt
+   # on big-endian it will work without it, but some features will be unavailable
    icudatadir <- file.path(R_PACKAGE_DIR, paste0('libs'))
    file.copy(icudtpath, icudatadir, overwrite=TRUE)
 }
