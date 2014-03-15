9634a1ccec578c3e3faa687510f1adb37c33c4eb
gagolews <egzaltowany@gmail.com>
2014-03-15 20:32:20 +0100

    automated benchmarks v2
    Former-commit-id: 2f2678a210caae27248303f25ceae37fc2fd7946

---
+++ devel/benchmarks/run_package_benchmarks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if test $# -eq 0
+then
+   RHOME=`R RHOME`
+else
+   RHOME="$1"
+fi
+
+$RHOME/bin/R CMD INSTALL --preclean .
+
+LC_ALL="pl_PL.UTF-8"      $RHOME/bin/Rscript \
+   --vanilla devel/benchmarks/run_package_benchmarks.R
+
+LC_ALL="pl_PL.iso-8859-2" $RHOME/bin/Rscript \
+   --vanilla devel/benchmarks/run_package_benchmarks.R
