1eaca8fa87d29df1558fac7f8e16ee3507193589
gagolews <egzaltowany@gmail.com>
2014-04-01 13:19:36 +0200

    new benchmark gathering method

---
+++ devel/benchmarks/rbmark-sort2.R
@@ -0,0 +1,30 @@
+benchmark_description <- "sorts a set of words of various lengths "%+%
+                         "(ASCII + Polish letters, native encoding) "%+%
+                         "[already sorted]"
+
+benchmark_do <- function() {
+   library('stringi')
+   library('stringr')
+
+   plletters <- stri_enc_fromutf32(list(97L, 98L, 99L, 100L, 101L, 102L, 103L,
+      104L, 105L, 106L, 107L,  108L, 109L, 110L, 111L, 112L, 113L, 114L, 115L,
+      116L, 117L, 118L,  119L, 120L, 121L, 122L, 261L, 263L, 281L, 322L, 324L,
+      243L, 347L,  378L, 380L))
+   plletters <- enc2native(plletters)
+
+   set.seed(123)
+   xrand <- replicate(10000, {
+      paste(sample(plletters,
+         floor(abs(rcauchy(1, 10))+1), replace=TRUE), collapse='')
+   })
+   xsort <- sort(xrand)
+
+   gc(reset=TRUE)
+   
+   benchmark2(
+      stri_sort(xsort),
+      stri_sort(xsort, opts_collator=NA),
+      sort(xsort),
+      replications=100L
+   )
+}
