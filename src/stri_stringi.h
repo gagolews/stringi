980165a860960ef113d5621340f90478910d184c
bartektartanus <bartektartanus@gmail.com>
2014-04-17 18:43:04 +0200

    new function - stri_duplicated_any
    Former-commit-id: 9d8a9d9516a4e4c16f3f16bbfa708681cedc1b20

--- src/stri_stringi.h
+++ src/stri_stringi.h
@@ -79,6 +79,7 @@ SEXP stri_order_or_sort(SEXP str, SEXP decreasing, SEXP na_last,
 
 SEXP stri_unique(SEXP str, SEXP collator_opts);
 SEXP stri_duplicated(SEXP str, SEXP fromLast, SEXP collator_opts);
+SEXP stri_duplicated_any(SEXP str, SEXP fromLast, SEXP collator_opts);
 
 // ICU_settings.cpp:
 SEXP stri_info();
