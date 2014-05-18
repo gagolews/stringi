971e8a8ce9c6a76b9f019ea7a49c6b21ea60c809
gagolews <egzaltowany@gmail.com>
2014-05-18 23:24:46 +0200

    playing with transliteration and msg formatting
    Former-commit-id: 291f029f1b4ba6200b7fed9bc43eeef7269a4c44

---
+++ R/rbnf.R
@@ -0,0 +1,85 @@
+## This file is part of the 'stringi' package for R.
+## Copyright (c) 2013-2014, Marek Gagolewski and Bartek Tartanus
+## All rights reserved.
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+##
+## 1. Redistributions of source code must retain the above copyright notice,
+## this list of conditions and the following disclaimer.
+##
+## 2. Redistributions in binary form must reproduce the above copyright notice,
+## this list of conditions and the following disclaimer in the documentation
+## and/or other materials provided with the distribution.
+##
+## 3. Neither the name of the copyright holder nor the names of its
+## contributors may be used to endorse or promote products derived from
+## this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+## BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+## FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+## PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+## OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+## WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+## OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+## EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+#' @title
+#' t.b.d.
+#' 
+#' @description
+#' t.b.d.
+#' 
+#' @details
+#' t.b.d.
+#' 
+#' @param x numeric vector
+#' @param ruleset single string; one of \code{spellout}, \code{ordinal}, \code{duration},
+#'                 \code{numbering_system}?
+#' @param locale \code{NULL} or \code{""} for case mapping following
+#' the conventions of the default locale, or a single string with
+#' locale identifier, see \link{stringi-locale}.
+#' 
+#' @return
+#' Returns a character vector
+#' 
+#' @family number_format
+#' @export
+stri_num_rbnf_format <- function(x, ruleset='spellout', locale=NULL) # stri_format_rbnf?
+{
+   stop('t.b.d.')  
+}
+
+
+#' @title
+#' t.b.d.
+#' 
+#' @description
+#' t.b.d.
+#' 
+#' @details
+#' t.b.d.
+#' 
+#' @param str character vector
+#' @param ruleset single string; one of \code{spellout}, \code{ordinal}, \code{duration},
+#'                 \code{numbering_system}?
+#' @param locale \code{NULL} or \code{""} for case mapping following
+#' the conventions of the default locale, or a single string with
+#' locale identifier, see \link{stringi-locale}.
+#' @param lenient single logical value; ....
+#' 
+#' @return
+#' Returns a numeric vector (list of numeric vectors?)
+#' 
+#' @family number_format
+#' @export
+stri_num_rbnf_parse <- function(str, ruleset='spellout', locale=NULL, lenient=TRUE) # stri_parse_rbnf?
+{
+   stop('t.b.d.')
+}
+
