7640f4906345dee7fc2751fcf6882cf12459e4c8
bartektartanus <bartektartanus@gmail.com>
2014-07-25 01:02:22 +0200

    tests for stri_subset_fixed

---
+++ devel/testthat/test-subset-fixed.R
@@ -0,0 +1,22 @@
+require("testthat")
+
+test_that("stri_subset_fixed", {
+   expect_identical(stri_subset_fixed("a", NA), NA_character_)
+   expect_identical(stri_subset_fixed(NA, "a"), NA_character_)
+   expect_identical(stri_subset_fixed(NA, NA), NA_character_)
+   expect_identical(stri_subset_fixed(c("","ala"),"ala"), "ala")
+   expect_identical(stri_subset_fixed(c("ala","", "", "bbb"),c("ala", "bbb")), c("ala", "bbb"))
+   expect_identical(stri_subset_fixed("kot lorem1", character(0)), character(0))
+   expect_identical(stri_subset_fixed(character(0), "ipsum 1234"), character(0))
+   expect_identical(stri_subset_fixed(character(0), character(0)), character(0))
+   expect_identical(stri_subset_fixed(c("ab", "cab", "ccccab", "aaaabaaaa"), "ab"), c("ab", "cab", "ccccab", "aaaabaaaa"))
+   expect_identical(stri_subset_fixed(c("Lorem\n123", " ", "kota", "4\t\u0105"), c(" ", "\t\u0105")), "4\t\u0105")
+   expect_warning(stri_subset_fixed(rep("asd", 5), rep("a", 2)))
+   expect_identical(stri_subset_fixed("\u0104\u0105", stri_trans_nfkd("\u0104\u0105")), character(0))
+   expect_equivalent(stri_subset_fixed("aaaab", "ab"), "aaaab")
+   expect_equivalent(stri_subset_fixed("bababababaab", "aab"), "bababababaab")
+
+   expect_warning(expect_identical(stri_subset_fixed("",""), NA_character_))
+   expect_warning(expect_identical(stri_subset_fixed("a",""), NA_character_))
+   expect_warning(expect_identical(stri_subset_fixed("","a"), character(0)))
+})
