174144a4bcac240c38514a2c31ef4e78e17b37b9
bartektartanus <bartektartanus@gmail.com>
2014-07-25 02:15:30 +0200

    added tests for stri_subset_charclass
    Former-commit-id: 6f7ec1a7d95538681bffd4d44c2fc97ccc513956

---
+++ devel/testthat/test-subset-charclass.R
@@ -0,0 +1,17 @@
+require("testthat")
+
+test_that("stri_subset_charclass", {
+   expect_identical(stri_subset_charclass("a", NA), NA_character_)
+   expect_identical(stri_subset_charclass(NA, "\\p{Z}"), NA_character_)
+   expect_identical(stri_subset_charclass(NA, NA), NA_character_)
+   expect_error(stri_subset_charclass("a", "WTF????"))
+   expect_identical(stri_subset_charclass(c("","ala"),"\\p{L}"), "ala")
+   expect_identical(stri_subset_charclass(c("ala","", "", "111"),c("\\p{L}", "\\p{N}")), c("ala",111))
+
+   expect_identical(stri_subset_charclass(c("a", "ab", "abc", "1234"), "\\p{L}"), c("a", "ab", "abc"))
+   expect_identical(stri_subset_charclass("a\u0105bc", c("\\p{L}", "\\p{Ll}", "\\p{Lu}")), rep("a\u0105bc",2))
+   expect_identical(stri_subset_charclass("a\u0105bc", c("\\p{l}", "\\p{ll}", "\\p{lu}")), rep("a\u0105bc",2))
+   expect_identical(stri_subset_charclass("a\u0105bc", c("\\P{l}", "\\P{ll}", "\\P{lu}")), "a\u0105bc")
+   expect_identical(stri_subset_charclass("a\u0105bc", c("\\p{AlPh_a  bEtic}")), "a\u0105bc")
+   expect_identical(stri_subset_charclass("","\\p{L}"), character(0))
+})
