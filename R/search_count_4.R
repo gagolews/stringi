af55c652ee863e37551f1300c20e3ee3f542b672
gagolews <egzaltowany@gmail.com>
2014-10-21 22:38:51 +0200

    #103 ongoing: man pages for stri_count merged
    Former-commit-id: 4ce12e35a379d4f606e776fbfba4f74905e14f6c

--- R/search_count_4.R
+++ R/search_count_4.R
@@ -30,59 +30,68 @@
 
 
 #' @title
-#' Detect a Pattern Match
+#' Count the Number of Pattern Matches
 #'
 #' @description
-#' These functions determine, for each string in \code{str},
-#' if there is at least one match to a corresponding \code{pattern}.
+#' These functions count the number of occurrences
+#' of a pattern in a string.
 #'
 #' @details
 #' Vectorized over \code{str} and \code{pattern}.
 #' 
 #' If \code{pattern} is empty, then the result is \code{NA}
 #' and a warning is generated.
-#'
-#' \code{stri_detect} is a convenience function.
-#' It calls either \code{stri_detect_regex},
-#' \code{stri_detect_fixed}, \code{stri_detect_coll},
-#' or \code{stri_detect_charclass}, depending on the argument used.
+#' 
+#' \code{stri_count} is a convenience function.
+#' It calls either \code{stri_count_regex},
+#' \code{stri_count_fixed}, \code{stri_count_coll},
+#' or \code{stri_count_charclass}, depending on the argument used.
 #' Unless you are a very lazy person, please call the underlying functions
 #' directly for better performance.
-#'
 #'
 #' @param str character vector with strings to search in
 #' @param pattern,regex,fixed,coll,charclass character vector defining search patterns;
 #' for more details refer to \link{stringi-search}
 #' @param opts_regex a named list with \pkg{ICU} Regex settings
 #' as generated with \code{\link{stri_opts_regex}}; \code{NULL}
-#' for default settings; \code{stri_detect_regex} only
+#' for default settings; \code{stri_count_regex} only
 #' @param opts_collator a named list with \pkg{ICU} Collator's settings
 #' as generated with \code{\link{stri_opts_collator}}; \code{NULL}
-#' for default settings; \code{stri_detect_coll} only
+#' for default settings; \code{stri_count_coll} only
 #' @param ... additional arguments passed to the underlying functions;
-#' \code{stri_detect} only
+#' \code{stri_count} only
 #'
-#' @return All the functions return a logical vector.
+#' @return All the functions return an integer vector.
 #'
 #' @examples
 #' \donttest{
-#' stri_detect_fixed(c("stringi R", "REXAMINE", "123"), c('i', 'R', '0'))
-#' stri_detect_fixed(c("stringi R", "REXAMINE", "123"), 'R')
+#' s <- "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+#' stri_count(s, fixed="dolor")
+#' stri_count(s, regex="\\p{L}+")
 #' 
-#' stri_detect_charclass(c("stRRRingi","REXAMINE","123"),
+#' stri_count_fixed(s, " ")
+#' stri_count_fixed(s, "o")
+#' stri_count_fixed(s, "it")
+#' stri_count_fixed(s, letters)
+#' stri_count_fixed("babab", "b")
+#' stri_count_fixed(c("stringi", "123"), "string")
+#' 
+#' stri_count_charclass(c("stRRRingi", "STrrrINGI", "123"),
 #'    c("\\p{Ll}", "\\p{Lu}", "\\p{Zs}"))
-#'    
-#' stri_detect_regex(c("stringi R", "REXAMINE","123"), 'R.')
-#' stri_detect_regex(c("stringi R", "REXAMINE","123"), '[[:alpha:]]*?')
-#' stri_detect_regex(c("stringi R", "REXAMINE","123"), '[a-zC1]')
-#' stri_detect_regex(c("stringi R", "REXAMINE","123"), '( R|RE)')
-#' stri_detect_regex("stringi", "STRING.", opts_regex=stri_opts_regex(case_insensitive=TRUE))
+#' stri_count_charclass(" \t\n", "\\p{WHITE_SPACE}") # white space - binary property
+#' stri_count_charclass(" \t\n", "\\p{Z}") # whitespace - general category (note the difference)
+#' 
+#' stri_count_regex(s, "(s|el)it")
+#' stri_count_regex(s, "i.i")
+#' stri_count_regex(s, ".it")
+#' stri_count_regex("bab baab baaab", c("b.*?b", "b.b"))
+#' stri_count_regex(c("stringi", "123"), "^(s|1)")
 #' }
 #'
-#' @family search_detect
+#' @family search_count
 #' @export
-#' @rdname stri_detect
-stri_detect <- function(str, ..., regex, fixed, coll, charclass) {
+#' @rdname stri_count
+stri_count <- function(str, ..., regex, fixed, coll, charclass) {
    providedarg <- c("regex"=!missing(regex), "fixed"    =!missing(fixed),
                     "coll" =!missing(coll),  "charclass"=!missing(charclass))
 
@@ -90,37 +99,40 @@ stri_detect <- function(str, ..., regex, fixed, coll, charclass) {
       stop("you have to specify either `regex`, `fixed`, `coll`, or `charclass`")
 
    if (providedarg["regex"])
-      stri_detect_regex(str, regex, ...)
+      stri_count_regex(str, regex, ...)
    else if (providedarg["fixed"])
-      stri_detect_fixed(str, fixed, ...)
+      stri_count_fixed(str, fixed, ...)
    else if (providedarg["coll"])
-      stri_detect_coll(str, coll, ...)
+      stri_count_coll(str, coll, ...)
    else if (providedarg["charclass"])
-      stri_detect_charclass(str, charclass, ...)
-}
-
-#' @export
-#' @rdname stri_detect
-stri_detect_fixed <- function(str, pattern) {
-   .Call("stri_detect_fixed", str, pattern, PACKAGE="stringi")
-}
-
-#' @export
-#' @rdname stri_detect
-stri_detect_charclass <- function(str, pattern) {
-   .Call("stri_detect_charclass", str, pattern, PACKAGE="stringi")
+      stri_count_charclass(str, charclass, ...)
 }
 
 
 #' @export
-#' @rdname stri_detect
-stri_detect_coll <- function(str, pattern, opts_collator=NULL) {
-   .Call("stri_detect_coll", str, pattern, opts_collator, PACKAGE="stringi")
+#' @rdname stri_count
+stri_count_charclass <- function(str, pattern) {
+   .Call("stri_count_charclass", str, pattern, PACKAGE="stringi")
 }
 
 
 #' @export
-#' @rdname stri_detect
-stri_detect_regex <- function(str, pattern, opts_regex=NULL) {
-   .Call("stri_detect_regex", str, pattern, opts_regex, PACKAGE="stringi")
+#' @rdname stri_count
+stri_count_coll <- function(str, pattern, opts_collator=NULL) {
+   .Call("stri_count_coll", str, pattern, opts_collator, PACKAGE="stringi")
 }
+
+
+#' @export
+#' @rdname stri_count
+stri_count_fixed <- function(str, pattern) {
+   .Call("stri_count_fixed", str, pattern, PACKAGE="stringi")
+}
+
+
+#' @export
+#' @rdname stri_count
+stri_count_regex <- function(str, pattern, opts_regex=NULL) {
+   .Call("stri_count_regex", str, pattern, opts_regex, PACKAGE="stringi")
+}
+
