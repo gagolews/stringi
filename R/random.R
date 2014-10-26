e29e797cd3ddd34e316a776eecbbf9f8ab5780e8
gagolews <egzaltowany@gmail.com>
2014-10-26 15:52:54 +0100

    #77 done: stri_rand_lipsum added

--- R/random.R
+++ R/random.R
@@ -109,3 +109,115 @@ stri_rand_shuffle <- function(str) {
 stri_rand_strings <- function(n, length, pattern="[A-Za-z0-9]") {
    .Call("stri_rand_strings", n, length, pattern, PACKAGE="stringi")
 }
+
+
+#' @title
+#' A Lorem Ipsum Generator
+#'
+#' @description
+#' Generates (pseudo)random \emph{lorem ipsum} text consisting
+#' of a given number of text paragraphs.
+#'
+#' @details
+#' \emph{Lorem ipsum} is a dummy text often used as a source
+#' of data for string processing and displaying/layouting exercises.
+#' 
+#' Current implementation is very simple:
+#' words are selected randomly from a Zipf distribution 
+#' (we base on a set of ca. 190 predefined Latin words).
+#' Number of words per sentence and sentences per paragraph
+#' follows a distretized, truncated normal distribution.
+#' No Markov chain modeling, just i.i.d. word selection.
+#'
+#' @param nparagraphs single integer, number of paragraphs to generate
+#' @param start_lipsum single logical value; should the resulting
+#' text start with \emph{Lorem ipsum dolor sit amet}?
+#'
+#' @return Returns a character vector of length \code{nparagraphs}.
+#'
+#' @examples
+#' \donttest{
+#' cat(sapply(
+#'    stri_wrap(stri_rand_lipsum(10), 80, simplify=FALSE),
+#'    stri_flatten, collapse="\n"), sep="\n\n")
+#' cat(stri_rand_lipsum(10), sep="\n\n")
+#' }
+#'
+#' @family random
+#' @export
+stri_rand_lipsum <- function(nparagraphs, start_lipsum=TRUE) {
+   # Whoa! A pure R function in stringi :)
+   # Version 0.3-1 (Marek Gagolewski, 2014-10-16)
+   
+   nparagraphs <- as.integer(nparagraphs)
+   stopifnot(is.finite(nparagraphs), nparagraphs >= 1)
+   start_lipsum <- identical(start_lipsum, TRUE)
+   
+   rwords <- function(n) { # generate n random words
+      words <- c("SED", "IN", "UT", "ET", "AC", "EU", "NON", "NEC", "AMET",  
+         "SIT", "VEL", "AT", "MAURIS", "A", "VITAE", "EGET", "QUIS", "NUNC",  
+         "NULLA", "ID", "VESTIBULUM", "PELLENTESQUE", "TINCIDUNT", "ALIQUAM",  
+         "IPSUM", "DONEC", "TURPIS", "LIGULA", "EGESTAS", "NIBH", "SAPIEN",  
+         "ANTE", "NISL", "VELIT", "ERAT", "EROS", "LEO", "MAGNA", "JUSTO", 
+         "ENIM", "MI", "PURUS", "EST", "LACUS", "LOREM", "QUAM", "DIAM",  
+         "RISUS", "DOLOR", "SEM", "AUGUE", "NEQUE", "TEMPOR", "DUI", "ARCU",  
+         "METUS", "TORTOR", "URNA", "LIBERO", "PHARETRA", "TEMPUS", "FAUCIBUS",  
+         "LECTUS", "SUSPENDISSE", "FELIS", "ODIO", "ORCI", "VARIUS", "MASSA",  
+         "TELLUS", "VOLUTPAT", "BLANDIT", "INTERDUM", "LOBORTIS", "MAXIMUS",  
+         "NISI", "LUCTUS", "PORTTITOR", "AUCTOR", "ELEMENTUM", "EX", "MAECENAS",  
+         "MALESUADA", "TRISTIQUE", "ULLAMCORPER", "ULTRICES", "NULLAM",  
+         "CONSEQUAT", "LACINIA", "PHASELLUS", "ACCUMSAN", "DAPIBUS", "ELEIFEND",  
+         "COMMODO", "DUIS", "EFFICITUR", "ELIT", "IMPERDIET", "AENEAN",  "IACULIS",
+         "NAM", "CONSECTETUR", "FERMENTUM", "PORTA", "SCELERISQUE",  "SODALES", 
+         "FEUGIAT", "LAOREET", "VULPUTATE", "DICTUM", "QUISQUE",  "FACILISIS", 
+         "FINIBUS", "ORNARE", "PULVINAR", "RHONCUS", "CONDIMENTUM",  "MOLLIS", 
+         "PRETIUM", "ALIQUET", "CONGUE", "POSUERE", "SUSCIPIT",  "ULTRICIES", 
+         "CURABITUR", "GRAVIDA", "MATTIS", "VIVERRA", "CURSUS",  "EUISMOD", 
+         "RUTRUM", "VENENATIS", "CONVALLIS", "PROIN", "VEHICULA",  "PLACERAT", 
+         "SAGITTIS", "CRAS", "INTEGER", "MORBI", "VIVAMUS",  "PRAESENT", 
+         "BIBENDUM", "MOLESTIE", "SEMPER", "FRINGILLA", "FUSCE",  "DIGNISSIM", 
+         "ETIAM", "HENDRERIT", "SOLLICITUDIN", "PER", "FAMES",  "POTENTI", "AD", 
+         "APTENT", "CLASS", "CONUBIA", "HIMENAEOS", "INCEPTOS",  "LITORA", 
+         "NOSTRA", "SOCIOSQU", "TACITI", "TORQUENT", "HABITANT",  "NETUS", 
+         "SENECTUS", "PRIMIS", "CUM", "DIS", "MAGNIS", "MONTES",  "MUS", 
+         "NASCETUR", "NATOQUE", "PARTURIENT", "PENATIBUS", "RIDICULUS",  
+         "SOCIIS", "ADIPISCING", "FACILISI", "CUBILIA", "CURAE", "DICTUMST",  
+         "HABITASSE", "HAC", "PLATEA")
+      
+      # Zipf distribution
+      dzipf <- function(k, N, s) 1/k^s/sum(1/(1:N)^s)
+      pzipf.y <- c(0, cumsum(dzipf(1:length(words), length(words), 0.5)))
+      robs <- findInterval(runif(n), pzipf.y)
+      words[robs]
+   }
+   
+   
+   rtruncnorm <- function(n, a, b, mu, sd) {
+      # truncated discretized normal distribution
+      x <- round(rnorm(n, mu, sd))
+      while(any(x < a | x > b))
+         x[x < a | x > b] <- round(rnorm(sum(x < a | x > b), mu, sd))
+      x
+   }
+   
+   sent_para <- rtruncnorm(nparagraphs, 7, 20, 11, 3)
+   word_sent <- lapply(sent_para, function(numsent)
+      rtruncnorm(numsent, 2, Inf, 8, 3))
+   
+   totwords <- sum(unlist(word_sent))
+   words <- rwords(totwords)
+   seps <- sample(c(" ", ", "), replace=TRUE, size=totwords, prob=c(0.9, 0.1))
+   seps[cumsum(unlist(word_sent))] <- sample(c(". ", "? ", "! "),
+      size=length(unlist(word_sent)), replace=TRUE, prob=c(0.9, 0.05, 0.05)) # end of sentence
+   seps[cumsum(sapply(word_sent, sum))] <- ".\n" # end of para
+   seps[totwords] <- "." # very last sentence in very last para
+   
+   if (start_lipsum) {
+      words <- c("LOREM", "IPSUM", "DOLOR", "SIT", "AMET", words)
+      seps  <- c(" ",     " ",     " ",     " ",   ", ", seps)
+   }
+   
+   ret <- stri_split_charclass(stri_paste(words, seps, collapse=""), "[\\n]")[[1]]
+   ret <- stri_trans_totitle(ret, boundary="sentence")
+   ret
+}
