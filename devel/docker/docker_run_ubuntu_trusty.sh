7839dce0bd0808b8d23d13670d5958a84dd29cd3
gagolews <egzaltowany@gmail.com>
2015-06-25 17:27:24 +0200

    #173: docker testbeds: debian and ubuntu

---
+++ devel/docker/docker_run_ubuntu_trusty.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker run --rm -ti ubuntu:trusty /bin/bash -c "
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen                                             && \
+locale-gen en_US.utf8                                                                   && \
+/usr/sbin/update-locale LANG=en_US.UTF-8                                                && \
+echo 'deb http://cran.rstudio.com/bin/linux/ubuntu/ trusty/' >> /etc/apt/sources.list   && \
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 51716619E084DAB9               && \
+apt-get -y -qq update                                                                   && \
+apt-get -y -qq install git libicu-dev g++ r-base-dev pkg-config texlive                 && \
+`cat stringi_forcesystemicu.sh`                                                            \
+"
