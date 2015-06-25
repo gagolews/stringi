f7224bae4bf33b8ab3e97bf4e581ba2352cdd911
gagolews <egzaltowany@gmail.com>
2015-06-25 11:55:13 +0200

    remove src/icu52

--- devel/cat_icu_sources.sh
+++ devel/cat_icu_sources.sh
@@ -2,10 +2,10 @@
 
 # must be called within src/
 
-prefix <- 'icu52_'
+prefix <- 'icu55_'
 
 catsrc <- function(dir, ext) {
-   lst <- dir(paste0('icu52/', dir), glob2rx(paste0("*.", ext)), full.names=TRUE)
+   lst <- dir(paste0('icu55/', dir), glob2rx(paste0("*.", ext)), full.names=TRUE)
    cat(lst, sep=' \\\n', file=paste0(prefix, dir, "_", ext, ".txt"))
 }
 
