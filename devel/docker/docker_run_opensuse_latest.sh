480947bb3c50f620de0f8449b69a735d3aa211d5
gagolews <egzaltowany@gmail.com>
2015-06-28 21:13:44 +0200

    inc version number + docker exts
    Former-commit-id: 6f1c0b8621c490a6122aeb40c3590abbc19c2a5f

--- devel/docker/docker_run_opensuse_latest.sh
+++ devel/docker/docker_run_opensuse_latest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker run --rm -ti opensuse:latest /bin/bash -c "
-zypper --quiet -n update                                        && \
-zypper --quiet -n install libicu-devel R git gcc gcc-c++        && \
-`cat stringi_forcesystemicu.sh`                                    \
+zypper --quiet -n update                                                 && \
+zypper --quiet -n install texlive-latex libicu-devel R git gcc gcc-c++   && \
+`cat stringi_forcesystemicu.sh`                                             \
 "
--- devel/docker/stringi_forcesystemicu.sh
+++ devel/docker/stringi_forcesystemicu.sh
@@ -4,9 +4,12 @@ GIT_SSL_NO_VERIFY=true git clone https://github.com/Rexamine/stringi.git && \
 R CMD build stringi                                                      && \
 R CMD INSTALL stringi_*.tar.gz                                              \
    --configure-args="--disable-icu-bundle" --no-html                     && \
-R CMD check stringi_*.tar.gz                                             && \
 Rscript --vanilla -e "install.packages(c('stringr', 'testthat'),            \
    repos='http://cran.rstudio.com/', quiet=TRUE)"                        && \
-cd stringi                                                               && \
 LC_ALL="en_US.UTF-8" Rscript --vanilla -e                                   \
-   "source('devel/testthat/run_package_tests.R')"
+   "setwd('stringi'); source('devel/testthat/run_package_tests.R')"      && \
+R CMD INSTALL stringi_*.tar.gz                                              \
+   --configure-args="--disable-pkg-config" --no-html                     && \
+LC_ALL="en_US.UTF-8" Rscript --vanilla -e                                   \
+   "setwd('stringi'); source('devel/testthat/run_package_tests.R')"      && \
+R CMD check stringi_*.tar.gz                                             \
