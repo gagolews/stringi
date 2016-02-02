844af212e1c2ebf993893190f3dcfc55a68a787e
gagolews <egzaltowany@gmail.com>
2016-02-02 16:19:34 +0100

    copyright year 2016

--- src/stri_exports.h
+++ src/stri_exports.h
@@ -1,5 +1,5 @@
 /* This file is part of the 'stringi' package for R.
- * Copyright (C) 2013-2015, Marek Gagolewski and Bartek Tartanus
+ * Copyright (C) 2013-2016, Marek Gagolewski and Bartek Tartanus
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -85,7 +85,7 @@ SEXP stri_reverse(SEXP s);
 
 // sub.cpp
 SEXP stri_sub(SEXP str, SEXP from, SEXP to, SEXP length);
-SEXP stri_sub_replacement(SEXP str, SEXP from, SEXP to, SEXP length, SEXP na_omit, SEXP value);
+SEXP stri_sub_replacement(SEXP str, SEXP from, SEXP to, SEXP length, SEXP omit_na, SEXP value);
 
 // encoding_management.cpp:
 SEXP stri_enc_list();
