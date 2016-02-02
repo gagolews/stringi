bd0d32a30ff557b1ecb4660212ea2d3b66ed729f
gagolews <egzaltowany@gmail.com>
2016-02-02 16:19:34 +0100

    copyright year 2016
    Former-commit-id: 844af212e1c2ebf993893190f3dcfc55a68a787e

--- R/todo_search_in.R
+++ R/todo_search_in.R
@@ -1,5 +1,5 @@
 ## This file is part of the 'stringi' package for R.
-## Copyright (C) 2013-2015, Marek Gagolewski and Bartek Tartanus
+## Copyright (C) 2013-2016, Marek Gagolewski and Bartek Tartanus
 ## All rights reserved.
 ##
 ## Redistribution and use in source and binary forms, with or without
