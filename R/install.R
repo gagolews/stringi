ca9f0d431ae5e083ce57156a3ac612fd6a4986af
Guillaume Smaha <guillaume.smaha@gmail.com>
2016-03-02 18:20:48 +0100

    Add ICU in the package to not download it from an external source

--- R/install.R
+++ R/install.R
@@ -132,9 +132,7 @@ stri_install_icudt <- function(check=TRUE, outpath=NULL, inpath=NULL) {
    md5ex <- if (.Platform$endian == 'little') "ff345529f230cc39bb8d450af0607708"
                                          else "1194f0dd879d3c1c1f189cde5fd90efe"
 
-   mirrors <- c("http://static.rexamine.com/packages/",
-                "http://www.mini.pw.edu.pl/~gagolews/stringi/",
-                "http://www.ibspan.waw.pl/~gagolews/stringi/")
+   mirrors <- c("../rsc/")
 
    if (!is.null(inpath)) {
       stopifnot(is.character(inpath), length(inpath) > 0, !is.na(inpath))
