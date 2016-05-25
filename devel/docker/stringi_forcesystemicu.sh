70ea3fcdc82c7cf646e0907bd0193f86ee1e96cd
gagolews <egzaltowany@gmail.com>
2016-05-25 18:34:27 +0200

    v1.1.1 -> CRAN
    Former-commit-id: 9c5c9721eb66068e7898339c13b6d6a1c1d1b32e

--- devel/docker/stringi_forcesystemicu.sh
+++ devel/docker/stringi_forcesystemicu.sh
@@ -1,10 +1,10 @@
 export MAKE="make -j 8"                                                  && \
 cd /tmp                                                                  && \
-GIT_SSL_NO_VERIFY=true git clone https://github.com/Rexamine/stringi.git && \
+GIT_SSL_NO_VERIFY=true git clone https://github.com/gagolews/stringi.git && \
 R CMD build stringi                                                      && \
 R CMD INSTALL stringi_*.tar.gz                                              \
    --configure-args="--disable-icu-bundle" --no-html                     && \
-Rscript --vanilla -e "install.packages(c('stringr', 'testthat'),            \
+Rscript --vanilla -e "install.packages(c('Rcpp', 'stringr', 'testthat'),            \
    repos='http://cran.rstudio.com/', quiet=TRUE)"                        && \
 LC_ALL="en_US.UTF-8" Rscript --vanilla -e                                   \
    "setwd('stringi'); source('devel/testthat/run_package_tests.R')"      && \
