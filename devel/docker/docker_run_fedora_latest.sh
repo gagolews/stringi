e953831ff2f768b13cfdc8398035bf5676fc57c3
gagolews <egzaltowany@gmail.com>
2016-12-30 15:19:48 +0100

    #242 docker test config

--- devel/docker/docker_run_fedora_latest.sh
+++ devel/docker/docker_run_fedora_latest.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# MG: I had to add tar rpm because otherwise string PDF man cannot be built
+# MG: added the `tar` rpm to prevent PDF man build failure
 
 docker run --rm -ti fedora:latest /bin/bash -c "
-dnf upgrade -y -q                                                                       && \
+dnf upgrade -y -q --refresh                                                             && \
 dnf install -y -q libicu-devel R-devel git tar                                          && \
 `cat stringi_forcesystemicu.sh`                                                            \
 "
