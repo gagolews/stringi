ea6a48fe5676fe2830e8c32d10659dc2797b67a8
Mikko Korpela <mvkorpel@iki.fi>
2017-03-24 16:59:58 +0200

    R_forceSymbols() is R >= 3.0.0. Test R version before using it...
    ...as instructed in R-exts manual:
    https://cran.r-project.org/doc/manuals/r-devel/R-exts.html#Platform-and-vers
    
    The function is declared in <R_ext/Rdynload.h> since R-devel commit 60373
    https://github.com/wch/r-source/commit/7184abcce5ea82403674886dd30714726fed7
    
    Is not declared in R 2.15.3:
    https://github.com/wch/r-source/blob/tags/R-2-15-3/src/include/R_ext/Rdynloa
    
    Is declared in R 3.0.0:
    https://github.com/wch/r-source/blob/tags/R-3-0-0/src/include/R_ext/Rdynload
    
    Former-commit-id: 4322d4bf1cf59c8adfa4b87c967fa170c53dbc92

--- src/stri_stringi.cpp
+++ src/stri_stringi.cpp
@@ -306,7 +306,9 @@ extern "C" void R_init_stringi(DllInfo* dll)
 
    R_registerRoutines(dll, NULL, cCallMethods, NULL, NULL);
    R_useDynamicSymbols(dll, FALSE);
+#if defined(R_VERSION) && R_VERSION >= R_Version(3, 0, 0)
    R_forceSymbols(dll, TRUE);
+#endif
 
    const R_CallMethodDef* methods = cCallMethods;
    while (methods->name) {
