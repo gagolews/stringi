0949640b05584c3cb57c06a4bdb857672a70946d
gagolews <egzaltowany@gmail.com>
2017-05-12 20:20:54 +0200

    fix travis conf

--- R/trans_transliterate.R
+++ R/trans_transliterate.R
@@ -90,7 +90,8 @@
 #' stri_trans_general("stringi", "upper") # see stri_trans_toupper
 #' stri_trans_general("\u0104", "nfd; lower") # compound id; see stri_trans_nfd
 #' stri_trans_general("tato nie wraca ranki wieczory", "pl-pl_FONIPA")
-#' stri_trans_general("\u2620", "name") # character name
+#' stri_trans_general("\u2620", "any-name") # character name
+#' stri_trans_general("\\N{latin small letter a}", "name-any") # decode name
 #' stri_trans_general("\u2620", "hex") # to hex
 stri_trans_general <- function(str, id) {
    .Call(C_stri_trans_general, str, id)
