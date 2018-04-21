7a0aa6a00bc7047dee2b9dc726ad12b67a11610c
gagolews <egzaltowany@gmail.com>
2018-04-21 15:45:32 +0200

    windows patch
    Former-commit-id: 132fbd656d3233973c04f73407daa3f166e7409f

---
+++ devel/testthat/test-utils.R
@@ -0,0 +1,15 @@
+require(testthat)
+context("test-utils.R")
+
+test_that("stri_remove_empty", {
+
+   expect_equivalent(stri_remove_empty(stri_na2empty(c("a", NA, "", "b"))), c("a", "b"))
+   expect_equivalent(stri_remove_empty(c("a", NA, "", "b")), c("a", NA, "b"))
+   expect_equivalent(stri_remove_empty(c("a", NA, "", "b"), TRUE), c("a", "b"))
+})
+
+test_that("stri_na2empty", {
+
+   expect_equivalent(stri_na2empty(c("a", NA, "", "b")), c("a", "", "", "b"))
+})
+
