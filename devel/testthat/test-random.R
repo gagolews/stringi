bbdcf3d4fd7837582baf2ce43e78cf36a1060389
gagolews <egzaltowany@gmail.com>
2018-07-23 11:22:25 +0200

    test for #319
    Former-commit-id: c38b9fcbdc6b9ed12ec1759c72d26954e16ea7aa

--- devel/testthat/test-random.R
+++ devel/testthat/test-random.R
@@ -3,7 +3,7 @@ context("test-random.R")
 
 test_that("stri_rand_shuffle", {
    expect_identical({set.seed(123); stri_rand_shuffle(c("abcdefghi", NA, "", "a"))},
-      c("fhdbgciea", NA, "", "a"))
+      c("cheidfbag", NA, "", "a"))
    expect_identical(stri_rand_shuffle(character(0)), character(0))
    expect_identical(stri_length(stri_rand_shuffle(rep(stri_paste(letters, collapse=''), 10))), rep(26L, 10))
    expect_identical({set.seed(12346); stri_rand_shuffle("\u0105\u0104")},
