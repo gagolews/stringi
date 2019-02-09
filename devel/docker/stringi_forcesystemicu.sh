7fce85f69fc1ec77a40f281dd188d5b113b44444
gagolews <m.gagolewski@gmail.com>
2019-02-09 14:34:40 +0100

    fix #325
    Former-commit-id: 6cdf79c51785f7be0fec1f3ff507b5608777b69a

--- devel/docker/stringi_forcesystemicu.sh
+++ devel/docker/stringi_forcesystemicu.sh
@@ -3,13 +3,13 @@ cd /tmp                                                                   && \
 GIT_SSL_NO_VERIFY=true git clone https://github.com/gagolews/stringi.git  && \
 R CMD build stringi                                                       && \
 R CMD INSTALL stringi_*.tar.gz --no-docs                                     \
-   --configure-args="--disable-icu-bundle --enable-gcc-pedantic"          && \
+   --configure-args="--disable-pkg-config --enable-gcc-pedantic"          && \
 Rscript --vanilla -e "install.packages(c('Rcpp', 'stringr', 'testthat'),     \
    repos='http://cran.rstudio.com/', quiet=TRUE)"                         && \
 STRINGI_icudt_download_test="TRUE" LC_ALL="en_US.UTF-8" Rscript --vanilla -e \
    "setwd('stringi'); source('devel/testthat/run_package_tests.R')"       && \
 R CMD INSTALL stringi_*.tar.gz --no-docs                                     \
-   --configure-args="--disable-pkg-config --enable-gcc-pedantic"          && \
+   --configure-args="--disable-icu-bundle --enable-gcc-pedantic"          && \
 STRINGI_icudt_download_test="TRUE" LC_ALL="en_US.UTF-8" Rscript --vanilla -e \
    "setwd('stringi'); source('devel/testthat/run_package_tests.R')"       && \
 R CMD check stringi_*.tar.gz                                                 \
