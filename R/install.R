f0da36ec770278ea4a86246b2b4b963d8b6f35b7
gagolews <m.gagolewski@gmail.com>
2020-08-19 15:56:37 +1000

    clean up headers
    Former-commit-id: f5688fea1842aec1392712fb718bc060f942678d

--- R/install.R
+++ R/install.R
@@ -1,5 +1,5 @@
 ## This file is part of the 'stringi' package for R.
-## Copyright (c) 2013-209, Marek Gagolewski and other contributors.
+## Copyright (c) 2013-2020, Marek Gagolewski <https://www.gagolewski.com>
 ## All rights reserved.
 ##
 ## Redistribution and use in source and binary forms, with or without
