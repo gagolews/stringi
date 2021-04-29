3e0f6f8a745f9a543244bd868e7115698bdc3a90
gagolews <m.gagolewski@gmail.com>
2021-04-29 10:38:20 +1000

    v1.6.1 started
    Former-commit-id: 9bcf220f95b1b3db489516be9d572d61815971d5

--- R/wrap.R
+++ R/wrap.R
@@ -1,7 +1,7 @@
 # kate: default-dictionary en_US
 
 ## This file is part of the 'stringi' package for R.
-## Copyright (c) 2013-2020, Marek Gagolewski <https://www.gagolewski.com>
+## Copyright (c) 2013-2021, Marek Gagolewski <https://www.gagolewski.com>
 ## All rights reserved.
 ##
 ## Redistribution and use in source and binary forms, with or without
@@ -29,6 +29,7 @@
 ## WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 ## OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 ## EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 
 #' @title
