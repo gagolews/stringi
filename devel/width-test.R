5b559a9f8e185994e962f1fc6b75cc98e49dbc1c
gagolews <m.gagolewski@gmail.com>
2021-05-13 13:10:30 +1000

    #429: debugging the new stri_width
    Former-commit-id: a4a9a5a7c32036f22bcf0e5e1fb7d21ea10b0687

---
+++ devel/width-test.R
@@ -0,0 +1,22 @@
+codes <- 1:55295
+x <- stringi::stri_enc_fromutf32(as.list(codes))
+w1 <- nchar(x, type="width")
+# cat(w1, sep="\n", file="width-nchar.csv")
+w2 <- scan("devel/width-stringi-1.5.3.csv")
+w3 <- scan("devel/width-stringi-1.6.1.csv")
+w4 <- stringi::stri_width(x)
+# cat(w2, sep="\n",
+#     file=sprintf("width-stringi-%s.csv", as.character(packageVersion("stringi"))))
+
+# d <- which(w2 != w3)
+# cbind.data.frame(x[d], nchar=w1[d], `1.5.3`=w2[d], w3[d])
+
+d <- (w4 != w2)
+y <- cbind.data.frame(
+    code=codes[d],
+    ucode=stringi::stri_trans_general(x[d], "Any-Hex/C"),
+    char=x[d],
+    name=stringi::stri_trans_general(x[d], "Any-Name"),
+    nchar=w1[d], `1.5.3`=w2[d], `1.6.1`=w3[d], w4[d])
+
+write.csv(y, "devel/delme-width-diff.csv", row.names=FALSE)
