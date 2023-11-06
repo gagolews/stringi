593bd9e7c0f93f39224bba580286866f2adaa37a
gagolews <m.gagolewski@gmail.com>
2023-11-06 14:48:27 +1100

    more
    Former-commit-id: 36878214141e7651a1caf569a3f017a4fdfa60d4

--- .devel/tinytest/test-uloc.R
+++ .devel/tinytest/test-uloc.R
@@ -4,7 +4,7 @@ library("stringi")
 
 expect_true(length(stri_locale_list()) > 0)
 
-stri_locale_set("XX_YY")
+suppressMessages(stri_locale_set("XX_YY"))
 suppressMessages(expect_true(substr(stri_locale_set("pl_PL"), 1, 5) == "xx_YY"))
 suppressMessages(expect_true(substr(stri_locale_set("pl_PL"), 1, 5) == "pl_PL"))
 
