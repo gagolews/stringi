8af83f1d97563600073f4ff5fa9c55e02f3455c3
gagolews <m.gagolewski@gmail.com>
2025-02-03 14:41:14 +0100

    more
    Former-commit-id: 2ef5f972553d7a6839b7bd7e2493e2b73ced6c94

---
+++ src/stri_callables.h
@@ -0,0 +1,53 @@
+/* This file is part of the 'stringi' project.
+ * Copyright (c) 2013-2025, Marek Gagolewski <https://www.gagolewski.com/>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "stri_stringi.h"
+#include <R_ext/Rdynload.h>
+
+
+
+// the callables library is defined in stri_callables.cpp
+const extern R_CallMethodDef stri_callables[];
+
+
+/*
+Third-party packages can retrieve the following functions from
+stringi's shared library via a call to:
+
+#include <R_ext/Rdynload.h>
+R_GetCCallable("stringi", "function_name");
+
+If you would like to get access to any additional functions (e.g., from ICU),
+feel free to contact the maintainer of stringi.
+*/
+
+
+int stric_u_hasBinaryProperty(int c, int which);
